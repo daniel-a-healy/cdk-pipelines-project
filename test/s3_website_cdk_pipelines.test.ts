ce78172eb550925b020f326254317df94acb9e3e
Daniel Healy <daniel.addison.healy@gmail.com>
2022-12-11 12:50:45 -0500

    Initial commit

---
+++ test/s3_website_cdk_pipelines.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S3WebsiteCdkPipelines from '../lib/s3_website_cdk_pipelines-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s3_website_cdk_pipelines-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S3WebsiteCdkPipelines.S3WebsiteCdkPipelinesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
